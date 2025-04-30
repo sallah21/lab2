d82e5b5d451d802256e893421bb3b69aa58fe985
sallah21 <luckyluj24@gmail.com>
2025-04-30 13:27:23 +0200

    3 gui mode

--- vcs_run.sh
+++ vcs_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script is used to run the VCS simulation for a given design.
 vcs -f rtl_files.f -sverilog -debug_pp -l vcs.log -o vcs_simv -debug_access+all -kdb
-# Run the simulation
-./vcs_simv -sv -l vcs_run.log
+# Run the simulation in GUI mode
+./vcs_simv -sv -l vcs_run.log -gui
 # Check if the simulation was successful
 if [ $? -eq 0 ]; then
     echo "Simulation completed successfully."
