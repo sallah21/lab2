06dadec72177bbc266807705efe5f355646dd01b
sallah21 <luckyluj24@gmail.com>
2025-05-14 13:03:07 +0200

    tx fix

--- RTL/uart_tx.sv
+++ RTL/uart_tx.sv
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
 module uart_tx #(
-  parameter BAUD_RATE = 115_200,
-  parameter CLOCK_RATE = 50_000_000
+  parameter BAUD_RATE ,
+  parameter CLOCK_RATE
 ) (
   input wire i_clk,
   input wire i_rst,
@@ -37,40 +37,51 @@ always @(posedge i_clk or negedge i_rst) begin
     transmission <= 0;
     tx_out <= 1; // idle state for tx is high
   end
-  else if (i_baud_x16_en)  begin
+  else if (i_baud_x16_en) begin
     // State transition
     current_state <= next_state;
 
+
     // TX out driving logic
-
-
     case (current_state)
       IDLE: begin
         if (i_send_data) begin
-          next_state <= DATA;
+          next_state <= START;
           data_pointer <= 0;
+        end
+        tx_out <= 1; // idle state
+      end
 
-        end 
-        else if (next_state == DATA && current_state == IDLE) begin
-          tx_out <= 0; // start bit is low
+      START: begin
+
+        next_state <= DATA;
+        if (next_state == DATA) begin
+          // transmission <= 1; // transmission in progress
+          tx_out <= 0;
           transmission <= 1;
-        end 
+        end
       end
 
       DATA: begin
         tx_out <= i_data_in[data_pointer];
         if (data_pointer == 7) begin
           next_state <= STOP;
-        end 
+        end
         else begin
           data_pointer <= data_pointer + 1;
         end
+        if (next_state == STOP) begin
+          // transmission <= 0; // transmission complete
+          tx_out <= 0;
+        end
       end
+
       STOP: begin
         tx_out <= 1; // stop bit is high
         next_state <= IDLE;
         transmission <= 0; // transmission complete
       end
+
       default: begin
         next_state <= IDLE;
       end
